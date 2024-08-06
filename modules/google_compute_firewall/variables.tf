02ce5ac9708063efa422ffcd147ff9c628335271
Joseph Villarreal <jvillarreallopez@qarik.com>
2024-08-06 10:49:43 -0600

    Update firewall variables validation to include direction

--- modules/google_compute_firewall/variables.tf
+++ modules/google_compute_firewall/variables.tf
@@ -3,19 +3,19 @@ variable "firewalls" {
     project                 = string
     name                    = string
     network                 = string
-    priority                = number
+    allow                   = optional(object({ metadata = string, ports = list(string) }))
+    deny                    = optional(object({ protocol = string, ports = list(string) }))
     description             = optional(string)
     destination_ranges      = optional(list(string))
     direction               = string
     disabled                = optional(bool)
+    log_config              = optional(object({ metadata = string }))
+    priority                = number
     source_ranges           = optional(list(string))
     source_service_accounts = optional(list(string))
     source_tags             = optional(list(string))
     target_service_accounts = optional(list(string))
     target_tags             = optional(list(string))
-    allow                   = optional(object({ protocol = string, ports = list(string) }))
-    deny                    = optional(object({ protocol = string, ports = list(string) }))
-    metadata                = optional(string)
   }))
   default = []
 
@@ -24,8 +24,9 @@ variable "firewalls" {
       fw.name != null &&
       length(fw.name) >= 1 &&
       length(fw.name) <= 63 &&
-      can(regex("[a-z]([-a-z0-9]*[a-z0-9])?", fw.name))
+      can(regex("[a-z]([-a-z0-9]*[a-z0-9])?", fw.name)) &&
+      (fw.direction == "INGRESS" || fw.direction == "EGRESS")
     )])
-    error_message = "Validation error: Ensure 'name' is specified, 1-63 characters long, and matches the pattern '[a-z]([-a-z0-9]*[a-z0-9])?'."
+    error_message = "Validation error: Ensure 'name' is specified, 1-63 characters long, matches the pattern '[a-z]([-a-z0-9]*[a-z0-9])?', and 'direction' is either 'INGRESS' or 'EGRESS'."
   }
 }
